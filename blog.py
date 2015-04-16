baf56878f7ee771921c9c6de67a6ec8c49396bf1
airtrack <airtrack.sk@gmail.com>
2015-04-16 22:15:12 +0800

    Optimze mobile experience.

--- blog.py
+++ blog.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
 from post import get_posts_list, get_post_content
+from mobile.sniffer.detect import detect_mobile_browser
 
 import sys
 reload(sys)
@@ -11,22 +11,35 @@ sys.setdefaultencoding('utf-8')
 blog = Flask(__name__)
 posts_dir = 'posts'
 
+def is_mobile_request():
+    ua = request.user_agent.string
+    if ua and detect_mobile_browser(ua):
+        return True
+    else:
+        return False
+
 @blog.route('/')
 def index():
-    return render_template('index.html', posts = get_posts_list(posts_dir))
+    mobile = 'mobile_' if is_mobile_request() else ''
+    return render_template('index.html',
+            mobile = mobile, posts = get_posts_list(posts_dir))
 
 @blog.route('/posts/<year>/<month>/<day>/<post_name>/')
 def post(year, month, day, post_name):
     post_time = year + '-' + month + '-' + day
     content = get_post_content(posts_dir, post_time, post_name)
     if content:
-        return render_template('post.html', title = post_name, time = post_time, content = content)
+        mobile = 'mobile_' if is_mobile_request() else ''
+        return render_template('post.html',
+                mobile = mobile, title = post_name,
+                time = post_time, content = content)
     else:
         return render_template('404.html'), 404
 
 @blog.route('/about.html')
 def about():
-    return blog.send_static_file('about.html')
+    mobile = 'mobile_' if is_mobile_request() else ''
+    return render_template('about.html', mobile = mobile)
 
 @blog.route('/<css_file>.css')
 def css(css_file):
